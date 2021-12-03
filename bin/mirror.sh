9329e1be8fe17e17d48d7cb4af4aa02cc93d2a5e
Tony Bowden <tony@tmtm.com>
2021-12-03 17:29:09 +0200

    script to mirror official site

---
+++ bin/mirror.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+rm -rf mirror
+mkdir mirror
+cd mirror
+
+CURLOPTS='-L -c /tmp/cookies -A eps/1.2'
+
+curl $CURLOPTS -o index $(jq -r .source.url ../meta.json)
+
+for url in $(nokogiri -e "puts @doc.css('#menu-item-7 a/@href').map(&:text)" index); do
+  echo $url
+  curl $CURLOPTS -o $(basename $url).html $url
+done
+
+cd -
